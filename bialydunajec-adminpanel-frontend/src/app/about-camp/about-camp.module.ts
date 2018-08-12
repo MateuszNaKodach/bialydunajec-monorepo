7f6bb10e59bdb3fe3c77ce173bf2fcd1c0fcc28d
mnowak <mateusz.nowak@intenso.pl>
2018-08-13 00:05:52 +0200

    Generate basics of FaqComponent.

--- bialydunajec-adminpanel-frontend/src/app/about-camp/about-camp.module.ts
+++ bialydunajec-adminpanel-frontend/src/app/about-camp/about-camp.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FaqComponent } from './faq/faq.component';
 import {SharedModule} from '../shared/shared.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {SuiAccordionModule} from 'ng2-semantic-ui';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    FlexLayoutModule,
+    SuiAccordionModule
   ],
   declarations: [FaqComponent]
 })
