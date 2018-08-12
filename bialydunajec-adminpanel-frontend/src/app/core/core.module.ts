ebf22371069148393782cda9b847cc2a66417659
mnowak <mateusz.nowak@intenso.pl>
2018-08-12 23:17:42 +0200

    Add HeaderComponent with style.

--- bialydunajec-adminpanel-frontend/src/app/core/core.module.ts
+++ bialydunajec-adminpanel-frontend/src/app/core/core.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule
   ],
   exports: [
     HeaderComponent
