b3279671e6c6143ef88d68e0b7bf67cabaf25802
mnowak <mateusz.nowak@intenso.pl>
2018-08-12 23:17:42 +0200

    Add HeaderComponent with style.

--- bialydunajec-angular-frontend/src/app/app.module.ts
+++ bialydunajec-angular-frontend/src/app/app.module.ts
@@ -4,6 +4,8 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CoreModule} from './core/core.module';
+import {SemanticUiModule} from './semantic-ui/semantic-ui.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import {CoreModule} from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    SemanticUiModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
