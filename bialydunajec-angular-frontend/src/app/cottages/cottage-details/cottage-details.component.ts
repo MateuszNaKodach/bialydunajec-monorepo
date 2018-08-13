82e9cda277632409a6556ebdb36bb327975c5f44
mnowak <mateusz.nowak@intenso.pl>
2018-08-13 15:03:05 +0200

    Polish header links.

---
+++ bialydunajec-angular-frontend/src/app/cottages/cottage-details/cottage-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bda-cottage-details',
+  templateUrl: './cottage-details.component.html',
+  styleUrls: ['./cottage-details.component.scss']
+})
+export class CottageDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- bialydunajec-angular-frontend/src/app/cottages/cottages.module.ts
+++ bialydunajec-angular-frontend/src/app/cottages/cottages.module.ts
@@ -1,18 +1,18 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {AcademicMinistrySelectionComponent} from './academic-ministry-selection/academic-ministry-selection.component';
-import {AcademicMinistryNamesListComponent} from './academic-ministry-names-list/academic-ministry-names-list.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {AcademicMinistriesComponent} from './academic-ministries/academic-ministries.component';
 import {SharedModule} from '../shared/shared.module';
 import { AcademicMinistryCardComponent } from './academic-ministry-card/academic-ministry-card.component';
+import { CottageDetailsComponent } from './cottage-details/cottage-details.component';
+import { CottagesNamesListComponent } from './cottage-details/cottages-names-list/cottages-names-list.component';
 
 @NgModule({
   imports: [
     SharedModule,
     FlexLayoutModule
   ],
-  declarations: [AcademicMinistrySelectionComponent, AcademicMinistryNamesListComponent, AcademicMinistriesComponent, AcademicMinistryCardComponent],
+  declarations: [AcademicMinistrySelectionComponent, AcademicMinistriesComponent, AcademicMinistryCardComponent, CottageDetailsComponent, CottagesNamesListComponent],
   exports: [AcademicMinistriesComponent]
 })
 export class CottagesModule {
