797768631e3c3ff4b8a99b189262cc1953b815be
mnowak <mateusz.nowak@intenso.pl>
2018-08-13 12:06:19 +0200

    Add cottages module.

---
+++ bialydunajec-angular-frontend/src/app/cottages/academic-ministry-selection/academic-ministry-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bda-academic-ministry-selection',
+  templateUrl: './academic-ministry-selection.component.html',
+  styleUrls: ['./academic-ministry-selection.component.scss']
+})
+export class AcademicMinistrySelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
