73dd9fe5c01ae5fe1d6d03f86bb9626257b61851
mnowak <mateusz.nowak@intenso.pl>
2018-08-13 11:55:55 +0200

    Finish very basic FAQ version.

--- bialydunajec-angular-frontend/src/app/about-camp/faq/faq.component.ts
+++ bialydunajec-angular-frontend/src/app/about-camp/faq/faq.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output} from '@angular/core';
+import {FaqCategory} from './faq-category.model';
 
 @Component({
   selector: 'bda-faq',
@@ -7,7 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqComponent implements OnInit {
 
-  constructor() { }
+  @Output() categories: FaqCategory[] = [
+    {
+      name: 'Zapisy na obóz',
+      questions: [
+        {content: 'Kto może jechać na obóz?', answer: 'Answer'},
+        {content: 'Czy trzeba być członkiem duszpasterstwa, żeby zapisać się na obóz?', answer: 'Answer'}
+      ]
+    },
+    {
+      name: 'Opłaty i inne kwestie finansowe',
+      questions: [
+        {content: 'Kto może jechać na obóz?', answer: 'Answer'},
+        {content: 'Czy trzeba być członkiem duszpasterstwa, żeby zapisać się na obóz?', answer: 'Answer'},
+        {content: 'Czy trzeba być członkiem duszpasterstwa, żeby zapisać się na obóz?', answer: 'Answer'},
+        {content: 'Czy trzeba być członkiem duszpasterstwa, żeby zapisać się na obóz?', answer: 'Answer'},
+        {content: 'Czy trzeba być członkiem duszpasterstwa, żeby zapisać się na obóz?', answer: 'Answer'}
+      ]
+    },
+    {
+      name: 'Warunki bytowe',
+      questions: [
+        {content: 'Kto może jechać na obóz?', answer: 'Answer'},
+        {content: 'Czy trzeba być członkiem duszpasterstwa, żeby zapisać się na obóz?', answer: 'Answer'},
+        {content: 'Czy trzeba być członkiem duszpasterstwa, żeby zapisać się na obóz?', answer: 'Answer'}
+      ]
+    },
+    {
+      name: 'Inne pytania',
+      questions: [
+        {content: 'Kto może jechać na obóz?', answer: 'Answer'},
+        {content: 'Czy trzeba być członkiem duszpasterstwa, żeby zapisać się na obóz?', answer: 'Answer'},
+        {content: 'Czy trzeba być członkiem duszpasterstwa, żeby zapisać się na obóz?', answer: 'Answer'},
+        {content: 'Czy trzeba być członkiem duszpasterstwa, żeby zapisać się na obóz?', answer: 'Answer'}
+      ]
+    }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
