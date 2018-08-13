7ffc69542605d5a31d7588b5f5a64184cc27f791
mnowak <mateusz.nowak@intenso.pl>
2018-08-13 15:03:05 +0200

    Polish header links.

---
+++ bialydunajec-adminpanel-frontend/src/app/cottages/cottage-details/cottages-names-list/cottages-names-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bda-cottages-names-list',
+  templateUrl: './cottages-names-list.component.html',
+  styleUrls: ['./cottages-names-list.component.scss']
+})
+export class CottagesNamesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
