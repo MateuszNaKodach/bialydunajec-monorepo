a62f0e6d14d0e3adc5d93e8a95520e1b8ffb8c07
mnowak <mateusz.nowak@intenso.pl>
2018-08-13 16:08:10 +0200

    Init dual toggle button.

--- bialydunajec-adminpanel-frontend/src/app/shared/shared.module.ts
+++ bialydunajec-adminpanel-frontend/src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SectionHeaderComponent} from './section-header/section-header.component';
+import { DualToggleButtonComponent } from './dual-toggle-button/dual-toggle-button.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SectionHeaderComponent],
-  exports: [CommonModule, SectionHeaderComponent]
+  declarations: [SectionHeaderComponent, DualToggleButtonComponent],
+  exports: [CommonModule, SectionHeaderComponent, DualToggleButtonComponent]
 })
 export class SharedModule {
 }
