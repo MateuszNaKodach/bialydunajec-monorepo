763999b9444905624e701686948b52c378ac250a
mnowak <mateusz.nowak@intenso.pl>
2018-08-13 13:17:57 +0200

    Add academin ministry card.

--- bialydunajec-adminpanel-frontend/src/app/app-routing.module.ts
+++ bialydunajec-adminpanel-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {FaqComponent} from './about-camp/faq/faq.component';
+import {AcademicMinistriesComponent} from './cottages/academic-ministries/academic-ministries.component';
 
 const routes: Routes = [
-  {path: '', component: FaqComponent}
+  {path: '', component: AcademicMinistriesComponent}
 ];
 
 @NgModule({
