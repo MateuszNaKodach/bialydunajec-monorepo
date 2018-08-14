2030b2e013d871ef51cf9f7571adce9b755fd53f
mnowak <mateusz.nowak@intenso.pl>
2018-08-14 13:38:42 +0200

    Add social links on cottage profile.

---
+++ bialydunajec-adminpanel-frontend/src/app/cottages/cottage/cottage-ministry-details/cottage-ministry-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bda-cottage-ministry-details',
+  templateUrl: './cottage-ministry-details.component.html',
+  styleUrls: ['./cottage-ministry-details.component.scss']
+})
+export class CottageMinistryDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
