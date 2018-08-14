6e9e30ca0bf1522bea2ce9ffe839766c2de627eb
mnowak <mateusz.nowak@intenso.pl>
2018-08-14 15:18:06 +0200

    Add descriptions for CottageDetails component.

---
+++ bialydunajec-angular-frontend/src/app/shared/camp-sign-up-button/camp-sign-up-button.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'bda-camp-sign-up-button',
+  templateUrl: './camp-sign-up-button.component.html',
+  styleUrls: ['./camp-sign-up-button.component.scss']
+})
+export class CampSignUpButtonComponent implements OnInit {
+
+  @Input() style: string;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
--- bialydunajec-angular-frontend/src/app/shared/shared.module.ts
+++ bialydunajec-angular-frontend/src/app/shared/shared.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SectionHeaderComponent} from './section-header/section-header.component';
 import { DualToggleButtonComponent } from './dual-toggle-button/dual-toggle-button.component';
+import { CampSignUpButtonComponent } from './camp-sign-up-button/camp-sign-up-button.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SectionHeaderComponent, DualToggleButtonComponent],
-  exports: [CommonModule, SectionHeaderComponent, DualToggleButtonComponent]
+  declarations: [SectionHeaderComponent, DualToggleButtonComponent, CampSignUpButtonComponent],
+  exports: [CommonModule, SectionHeaderComponent, DualToggleButtonComponent, CampSignUpButtonComponent]
 })
 export class SharedModule {
 }
