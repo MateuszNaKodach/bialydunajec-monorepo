1a41924ff0bd46c9fb0655d53d1cdda1cea8b101
mnowak <mateusz.nowak@intenso.pl>
2018-08-16 18:35:19 +0200

    #3 Add navigation between cottage and ministry tabs in CottageComponent.

--- bialydunajec-angular-frontend/src/app/cottages/cottage/cottage.component.ts
+++ bialydunajec-angular-frontend/src/app/cottages/cottage/cottage.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {OptionSelected} from '../../shared/dual-toggle-button/event/option-selected.event';
+
+const TAB_COTTAGE = 'Chatka';
+const TAB_MINISTRY = 'Duszpasterstwo';
 
 @Component({
   selector: 'bda-cottage',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CottageComponent implements OnInit {
 
-  constructor() { }
+  tabs = {
+    left: {
+      name: TAB_COTTAGE,
+      icon: 'user'
+    },
+    right: {
+      name: TAB_MINISTRY,
+      icon: 'blind'
+    }
+  };
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit() {
   }
 
+  isCottageTab(): boolean {
+    return this.router.url.indexOf(TAB_COTTAGE.toLowerCase()) !== -1;
+  }
+
+  isAcademicMinistryTab(): boolean {
+    return this.router.url.indexOf(TAB_MINISTRY.toLowerCase()) !== -1;
+  }
+
+  getSelectedTabSide() {
+    if (this.isCottageTab()) {
+      return 'left';
+    } else if (this.isAcademicMinistryTab()) {
+      return 'right';
+    } else {
+      return '';
+    }
+  }
+
+  onTabSelected($event: OptionSelected) {
+    const selectedOptionName = $event.name;
+    console.log(selectedOptionName);
+    this.router.navigate([selectedOptionName.toLowerCase()], {relativeTo: this.activatedRoute});
+  }
 }
