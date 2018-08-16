389c3e3c0befeb189d81b396cb1cd2ef0164acda
mnowak <mateusz.nowak@intenso.pl>
2018-08-16 22:56:39 +0200

    #5 Add components for car transport info.

---
+++ bialydunajec-adminpanel-frontend/src/app/transport/component/mean-of-transport/mean-of-transport.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'bda-mean-of-transport',
+  templateUrl: './mean-of-transport.component.html',
+  styleUrls: ['./mean-of-transport.component.scss']
+})
+export class MeanOfTransportComponent implements OnInit {
+
+  @Input() mapType: 'square' | 'rectangle' = 'square';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
