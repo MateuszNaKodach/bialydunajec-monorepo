65552668534fac0e2134dfcf8b7b2308ce92c254
mnowak <mateusz.nowak@intenso.pl>
2018-08-16 15:32:09 +0200

    #3 Externalize reusable component for cards with photo on one side.

--- bialydunajec-adminpanel-frontend/src/app/cottages/academic-ministries/academic-ministries.component.ts
+++ bialydunajec-adminpanel-frontend/src/app/cottages/academic-ministries/academic-ministries.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'bda-academic-ministries',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcademicMinistriesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit() {
+  }
+
+  onMinistrySelected(academicMinistry: { id: string, name: string }) {
+    this.router.navigate([academicMinistry.id + '/chatka'], {relativeTo: this.activatedRoute});
   }
 
 }
--- bialydunajec-adminpanel-frontend/src/app/cottages/academic-ministry-card/academic-ministry-card.component.ts
+++ bialydunajec-adminpanel-frontend/src/app/cottages/academic-ministry-card/academic-ministry-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {AcademicMinistryCard} from './academic-ministry-card.model';
 
 @Component({
@@ -8,12 +8,18 @@ import {AcademicMinistryCard} from './academic-ministry-card.model';
 })
 export class AcademicMinistryCardComponent implements OnInit {
 
+  @Output() click = new EventEmitter<void>();
+
   @Input() academicMinistry: AcademicMinistryCard;
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  onClick() {
+    this.click.emit();
   }
 
 }
