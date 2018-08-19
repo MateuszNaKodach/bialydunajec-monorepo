f56d00f2df31abe50df294b79e79deeee9ddd35f
mnowak <mateusz.nowak@intenso.pl>
2018-08-19 23:41:17 +0200

    #7 Add model classes for contact module.

---
+++ bialydunajec-adminpanel-frontend/src/app/contact/model/cadre-member.model.ts
@@ -0,0 +1,57 @@
+export class CadreMember {
+  id: string;
+  firstName: string;
+  lastName: string;
+  responsibility: string;
+  academicMinistryName: string;
+  contactInfo: ContactInfo;
+  description: PersonalDescription;
+
+  constructor(id: string,
+              firstName: string,
+              lastName: string,
+              responsibility: string,
+              academicMinistryName: string,
+              contactInfo: ContactInfo,
+              description: PersonalDescription = PersonalDescription.empty()) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.responsibility = responsibility;
+    this.academicMinistryName = academicMinistryName;
+    this.contactInfo = contactInfo;
+    this.description = description;
+  }
+}
+
+export class ContactInfo {
+  email: string;
+  phoneNumber: string;
+  facebookUrl: string;
+
+
+  constructor(email: string = '', phoneNumber: string = '', facebookUrl: string = '') {
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.facebookUrl = facebookUrl;
+  }
+}
+
+export class PersonalDescription {
+  title: string;
+  content: string;
+
+
+  constructor(title: string, content: string) {
+    this.title = title;
+    this.content = content;
+  }
+
+  static empty() {
+    return new PersonalDescription('', '');
+  }
+
+  isEmpty() {
+    return this.content == null || this.content.length === 0;
+  }
+}
