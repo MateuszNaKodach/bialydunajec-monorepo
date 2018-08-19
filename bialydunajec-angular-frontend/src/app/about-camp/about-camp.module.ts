9797db02f412f3931148a5318ec9d885e729b1de
mnowak <mateusz.nowak@intenso.pl>
2018-08-19 22:30:11 +0200

    #7 Add AboutCampRoutingModule

--- bialydunajec-angular-frontend/src/app/about-camp/about-camp.module.ts
+++ bialydunajec-angular-frontend/src/app/about-camp/about-camp.module.ts
@@ -1,19 +1,22 @@
-import { NgModule } from '@angular/core';
-import { FaqComponent } from './component/faq/faq.component';
+import {NgModule} from '@angular/core';
+import {FaqComponent} from './component/faq/faq.component';
 import {SharedModule} from '../shared/shared.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {SuiAccordionModule, SuiSearchModule} from 'ng2-semantic-ui';
-import { FaqCategoryComponent } from './component/faq/faq-category/faq-category.component';
+import {FaqCategoryComponent} from './component/faq/faq-category/faq-category.component';
 import {FaqService} from './service/faq.service';
+import {AboutCampRoutingModule} from './about-camp-routing.module';
 
 @NgModule({
   imports: [
     SharedModule,
     FlexLayoutModule,
     SuiAccordionModule,
-    SuiSearchModule
+    SuiSearchModule,
+    AboutCampRoutingModule
   ],
   declarations: [FaqComponent, FaqCategoryComponent],
   providers: [FaqService]
 })
-export class AboutCampModule { }
+export class AboutCampModule {
+}
