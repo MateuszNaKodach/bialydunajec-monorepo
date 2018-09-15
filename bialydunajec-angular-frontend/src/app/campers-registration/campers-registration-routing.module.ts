27584a147d8dbef4e4a3253af5c58920b529ac1d
mnowak <mateusz.nowak@intenso.pl>
2018-09-15 14:25:54 +0200

    #13 Add routing to registration for "ZAPISZ SIĘ!" button.

--- bialydunajec-angular-frontend/src/app/campers-registration/campers-registration-routing.module.ts
+++ bialydunajec-angular-frontend/src/app/campers-registration/campers-registration-routing.module.ts
@@ -1,8 +1,18 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
+import {campersRegistrationRoutingPaths} from './campers-registration-routing.paths';
+import {RegistrationFormComponent} from './component/registration-form/registration-form.component';
 
 const campersRegistrationRoutes: Routes = [
-  {path: '', component: null}
+  {
+    path: campersRegistrationRoutingPaths.root,
+    redirectTo: campersRegistrationRoutingPaths.form,
+    pathMatch: 'full'
+  },
+  {
+    path: campersRegistrationRoutingPaths.form,
+    component: RegistrationFormComponent
+  }
 ];
 
 @NgModule({
---
+++ bialydunajec-angular-frontend/src/app/campers-registration/component/registration-form/registration-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bda-registration-form',
+  templateUrl: './registration-form.component.html',
+  styleUrls: ['./registration-form.component.scss']
+})
+export class RegistrationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
