c7e8ab6546e83c2866234b11628ba63b518c1f65
mnowak <mateusz.nowak@intenso.pl>
2018-09-16 15:48:25 +0200

    #13 Fix RegistrationFormStepper width.

---
+++ bialydunajec-angular-frontend/src/app/campers-registration/component/registration-form/form-navigation-buttons/form-navigation-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bda-form-navigation-buttons',
+  templateUrl: './form-navigation-buttons.component.html',
+  styleUrls: ['./form-navigation-buttons.component.scss']
+})
+export class FormNavigationButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
