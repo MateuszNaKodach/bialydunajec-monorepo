ed5c34bea2153e794b8f35c32552bbfa1ae7eb8d
mnowak <mateusz.nowak@intenso.pl>
2018-09-18 16:38:27 +0200

    #13 Add validation wrapper for all inputs in personal data form.

---
+++ bialydunajec-angular-frontend/src/app/shared/helper/angular-form.helper.ts
@@ -0,0 +1,13 @@
+import {FormGroup} from '@angular/forms';
+
+export class AngularFormHelper {
+  static markFormGroupTouched(formGroup: FormGroup) {
+    (<any>Object).values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
+
+      if (control.controls) {
+        this.markFormGroupTouched(control);
+      }
+    });
+  }
+}
