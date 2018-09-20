9eb63466cf2372a29b86d4f96759980abf658afc
mnowak <mateusz.nowak@intenso.pl>
2018-09-20 18:59:36 +0200

    #13 Add additionalContent property to mean of transport options.

--- bialydunajec-angular-frontend/src/app/campers-registration/model/transport-type.enum.ts
+++ bialydunajec-angular-frontend/src/app/campers-registration/model/transport-type.enum.ts
@@ -1,6 +1,6 @@
 export enum TransportType {
   PRIVATE_TRANSPORT = 'privateTransport',
-  PRIVATE_CAR = 'privateCar',
+  PRIVATE_CAR = 'privateCar', // TODO: To remove!
   PUBLIC_TRANSPORT = 'publicTransport',
   CAMP_TRANSPORT = 'campTransport',
   HITCH_HIKING = 'hitchHiking',
