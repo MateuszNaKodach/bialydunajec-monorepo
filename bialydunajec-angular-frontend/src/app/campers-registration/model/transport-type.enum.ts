ea8dae75d008f081f55a5525b8566b54de4b8cbd
mnowak <mateusz.nowak@intenso.pl>
2018-09-20 18:13:23 +0200

    #13 Add showing parking space info on TransportFormComponent.

--- bialydunajec-angular-frontend/src/app/campers-registration/model/transport-type.enum.ts
+++ bialydunajec-angular-frontend/src/app/campers-registration/model/transport-type.enum.ts
@@ -1,5 +1,6 @@
 export enum TransportType {
   PRIVATE_TRANSPORT = 'privateTransport',
+  PRIVATE_CAR = 'privateCar',
   PUBLIC_TRANSPORT = 'publicTransport',
   CAMP_TRANSPORT = 'campTransport',
   HITCH_HIKING = 'hitchHiking',
