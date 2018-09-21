427b528adef7726bf3f6633284f3ea2bdf2c8fad
mnowak <mateusz.nowak@intenso.pl>
2018-09-21 14:34:36 +0200

    #18 Add Ant Design for bialydunajec-admin.

--- bialydunajec-angular-frontend/projects/bialydunajec-admin/src/app/app.module.ts
+++ bialydunajec-angular-frontend/projects/bialydunajec-admin/src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, pl_PL } from 'ng-zorro-antd';
+import { registerLocaleData } from '@angular/common';
+import pl from '@angular/common/locales/pl';
+
+registerLocaleData(pl);
 
 @NgModule({
   declarations: [
@@ -10,9 +18,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: pl_PL }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
