158c44a72a26360810059d65b91af605cff6a546
mnowak <mateusz.nowak@intenso.pl>
2018-09-21 10:34:16 +0200

    #15 Add new library to project "bialydunajec-commons"

---
+++ bialydunajec-angular-frontend/projects/bialydunajec-commons/src/lib/bialydunajec-commons.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bda-commons-bialydunajec-commons',
+  template: `
+    <p>
+      bialydunajec-commons works!
+    </p>
+  `,
+  styles: []
+})
+export class BialydunajecCommonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ bialydunajec-angular-frontend/projects/bialydunajec-commons/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of bialydunajec-commons
+ */
+
+export * from './lib/bialydunajec-commons.service';
+export * from './lib/bialydunajec-commons.component';
+export * from './lib/bialydunajec-commons.module';
