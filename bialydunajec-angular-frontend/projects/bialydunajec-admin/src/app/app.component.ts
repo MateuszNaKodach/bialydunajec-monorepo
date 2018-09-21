3f9ec2c89a26c66b39e030a7cb2c799410df2c1d
mnowak <mateusz.nowak@intenso.pl>
2018-09-21 23:53:22 +0200

    #18 Adjust bialydunajec-admin theme.

--- bialydunajec-angular-frontend/projects/bialydunajec-admin/src/app/app.component.ts
+++ bialydunajec-angular-frontend/projects/bialydunajec-admin/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bda-admin-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
   title = 'bialydunajec-admin';
