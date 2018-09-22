45b38735058390c854a4c59d7aa9fd334f6a8c45
mnowak <mateusz.nowak@intenso.pl>
2018-09-22 13:43:52 +0200

    #18 Move FormInputAbstractComponent to library bialydunajec-commons.

--- bialydunajec-adminpanel-frontend/projects/bialydunajec-commons/src/public_api.ts
+++ bialydunajec-adminpanel-frontend/projects/bialydunajec-commons/src/public_api.ts
@@ -5,3 +5,13 @@
 export * from './lib/bialydunajec-commons.service';
 export * from './lib/bialydunajec-commons.component';
 export * from './lib/bialydunajec-commons.module';
+
+
+export * from './lib/forms/bialydunajec-forms.module';
+export * from './lib/forms/abstract-component/form-input/form-input.abstract-component';
+export * from './lib/forms/config/error-defs.default';
+export * from './lib/forms/model/form-status.enum';
+
+
+
+
--- bialydunajec-adminpanel-frontend/projects/bialydunajec-main/src/app/campers-registration/service/camper-registration-form-state.service.ts
+++ bialydunajec-adminpanel-frontend/projects/bialydunajec-main/src/app/campers-registration/service/camper-registration-form-state.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {StepId} from '../component/registration-form/registration-form.config';
-import {FormStatus} from '../../shared/model/form-status.enum';
+import {FormStatus} from 'bialydunajec-commons';
 import {Subject} from 'rxjs';
 
 @Injectable()
