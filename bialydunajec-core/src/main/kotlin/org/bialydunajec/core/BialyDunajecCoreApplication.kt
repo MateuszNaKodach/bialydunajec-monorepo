410bde5ede7084ffef871a01933f5a9bc8f3335c
mnowak <mateusz.nowak@intenso.pl>
2018-09-24 11:46:52 +0200

    #1 Add bialydunajec-core gradle module.

---
+++ bialydunajec-core/src/main/kotlin/org/bialydunajec/core/BialyDunajecCoreApplication.kt
@@ -0,0 +1,11 @@
+package org.bialydunajec.core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BialyDunajecCoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<BialyDunajecCoreApplication>(*args)
+}
