46185fdb07db9a6d1914a987ee51ed556710e4ba
mnowak <mateusz.nowak@intenso.pl>
2018-09-24 20:54:52 +0200

    #1 Add bialydunajec-registrations module.

---
+++ bialydunajec-registrations/src/main/kotlin/org/bialydunajec/registrations/BialydunajecRegistrationsApplication.kt
@@ -0,0 +1,11 @@
+package org.bialydunajec.registrations
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BialydunajecRegistrationsApplication
+
+fun main(args: Array<String>) {
+    runApplication<BialydunajecRegistrationsApplication>(*args)
+}
