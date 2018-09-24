b1706b77b864b2cd1a59c46bce30c7e394ccb9a1
mnowak <mateusz.nowak@intenso.pl>
2018-09-24 13:51:38 +0200

    #1 Add presentation module to bialydunajec-core.

--- bialydunajec-core/presentation/src/main/kotlin/org/bialydunajec/core/presentation/TestController.kt
+++ bialydunajec-core/presentation/src/main/kotlin/org/bialydunajec/core/presentation/TestController.kt
@@ -1,8 +1,9 @@
-package org.bialydunajec.core
+package org.bialydunajec.core.presentation
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
 @RequestMapping("/test")
---
+++ bialydunajec-core/presentation/src/main/kotlin/org/bialydunajec/core/presentation/TestService.kt
@@ -0,0 +1,9 @@
+package org.bialydunajec.core.presentation
+
+import org.springframework.stereotype.Service
+
+@Service
+class TestService {
+
+    fun helloWorld() = "Hello World"
+}
