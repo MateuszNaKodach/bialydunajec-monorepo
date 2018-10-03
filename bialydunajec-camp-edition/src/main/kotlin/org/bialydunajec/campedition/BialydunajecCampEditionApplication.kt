dbbea3c14e15f921af715e8ce326ed3948fc489b
mnowak <mateusz.nowak@intenso.pl>
2018-10-03 10:30:17 +0200

    #5 Add domain module for bialydunajec-camp-edition.

---
+++ bialydunajec-camp-edition/src/main/kotlin/org/bialydunajec/campedition/BialydunajecCampEditionApplication.kt
@@ -0,0 +1,11 @@
+package org.bialydunajec.campedition
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BialydunajecCampEditionApplication
+
+fun main(args: Array<String>) {
+    runApplication<BialydunajecCampEditionApplication>(*args)
+}
