6db9d0172d400e42051598e4c4c5486acd7be087
mnowak <mateusz.nowak@intenso.pl>
2018-10-04 13:04:25 +0200

    #20 Add redirects to panel if user is logged and vice versa.

--- bialydunajec-adminpanel-frontend/projects/bialydunajec-admin/src/app/auth/service/auth.service.ts
+++ bialydunajec-adminpanel-frontend/projects/bialydunajec-admin/src/app/auth/service/auth.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {TokenEndpoint} from './rest/token.endpoint';
 import {Subject} from 'rxjs';
-import {Router} from '@angular/router';
 import {AuthState} from './auth.state';
 
 const CURRENT_USER_TOKEN_KEY = 'org.bialydunajec.current_user_token';
@@ -11,11 +10,10 @@ const CURRENT_USER_TOKEN_KEY = 'org.bialydunajec.current_user_token';
 })
 export class AuthService {
 
-  private localStorage: Storage;
+  localStorage: Storage;
   private authenticationSubject = new Subject<AuthState>();
 
-  constructor(
-    private authRestCalls: TokenEndpoint) {
+  constructor(private authRestCalls: TokenEndpoint) {
   }
 
   login(usernameOrEmail: string, password: string, rememberMe: boolean) {
@@ -24,6 +22,7 @@ export class AuthService {
         tokenResponse => {
           localStorage.setItem(CURRENT_USER_TOKEN_KEY, JSON.stringify(tokenResponse));
           this.authenticationSubject.next(new AuthState(tokenResponse.current_user));
+          console.log(this.userOAuthToken);
         },
         response => {
           if (response.status === 401) {
@@ -36,6 +35,11 @@ export class AuthService {
       );
   }
 
+  logoutCurrentUser() {
+    localStorage.removeItem(CURRENT_USER_TOKEN_KEY);
+    this.authenticationSubject.next(new AuthState(null));
+  }
+
   get userAuthentication() {
     return this.authenticationSubject.asObservable();
   }
@@ -45,8 +49,11 @@ export class AuthService {
   }
 
   get userOAuthToken() {
-    return JSON.parse(this.localStorage.getItem(CURRENT_USER_TOKEN_KEY));
+    return JSON.parse(localStorage.getItem(CURRENT_USER_TOKEN_KEY));
   }
 
+  isAuthenticated(): boolean {
+    return this.userOAuthToken;
+  }
 
 }
