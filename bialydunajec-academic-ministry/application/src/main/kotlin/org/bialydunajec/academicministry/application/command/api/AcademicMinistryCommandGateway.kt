73af6d981c195a28840e8137116ed7ec836da1b1
mnowak <mateusz.nowak@intenso.pl>
2018-10-06 11:04:42 +0200

    #6 Add AcademicMinistryQueryGateway.

--- bialydunajec-academic-ministry/application/src/main/kotlin/org/bialydunajec/academicministry/application/command/api/AcademicMinistryCommandGateway.kt
+++ bialydunajec-academic-ministry/application/src/main/kotlin/org/bialydunajec/academicministry/application/command/api/AcademicMinistryCommandGateway.kt
@@ -3,7 +3,9 @@ package org.bialydunajec.academicministry.application.command.api
 import org.bialydunajec.academicministry.application.command.CreateAcademicMinistryApplicationService
 import org.bialydunajec.academicministry.application.command.UpdateAcademicMinistryApplicationService
 import org.bialydunajec.ddd.application.base.command.CommandGateway
+import org.springframework.stereotype.Service
 
+@Service
 class AcademicMinistryCommandGateway internal constructor(
         private val createAcademicMinistryApplicationService: CreateAcademicMinistryApplicationService,
         private val updateAcademicMinistryApplicationService: UpdateAcademicMinistryApplicationService
--- bialydunajec-academic-ministry/application/src/main/kotlin/org/bialydunajec/academicministry/application/query/readmodel/AcademicMinistryDomainModelReader.kt
+++ bialydunajec-academic-ministry/application/src/main/kotlin/org/bialydunajec/academicministry/application/query/readmodel/AcademicMinistryDomainModelReader.kt
@@ -3,7 +3,9 @@ package org.bialydunajec.academicministry.application.query.readmodel
 import org.bialydunajec.academicministry.application.query.api.AcademicMinistryQuery
 import org.bialydunajec.academicministry.domain.academicministry.AcademicMinistryId
 import org.bialydunajec.academicministry.domain.academicministry.AcademicMinistryRepository
+import org.springframework.stereotype.Component
 
+@Component
 internal class AcademicMinistryDomainModelReader(private val repository: AcademicMinistryRepository) {
 
     fun readFor(query: AcademicMinistryQuery.All) =
