6e45b46c28f40154bd754f55980b3963b0d7476c
mnowak <mateusz.nowak@intenso.pl>
2018-10-07 22:11:45 +0200

    #20 Add form for creating new Camp Edition.

--- bialydunajec-angular-frontend/projects/bialydunajec-admin/src/app/camp-edition/service/rest/request/create-camp-edition.request.ts
+++ bialydunajec-angular-frontend/projects/bialydunajec-admin/src/app/camp-edition/service/rest/request/create-camp-edition.request.ts
@@ -2,4 +2,10 @@ export class CreateCampEditionRequest {
   campEditionId: number;
   campEditionStartDate: Date;
   campEditionEndDate: Date;
+
+  constructor(campEditionId: number, campEditionStartDate: Date, campEditionEndDate: Date) {
+    this.campEditionId = campEditionId;
+    this.campEditionStartDate = campEditionStartDate;
+    this.campEditionEndDate = campEditionEndDate;
+  }
 }
