8035e57483284c3728f5fe961a7c9274a6d9ba5e
mnowak <mateusz.nowak@intenso.pl>
2018-10-08 12:00:19 +0200

    #20 Add showing notification after try of creating camp edition.

--- bialydunajec-adminpanel-frontend/projects/bialydunajec-admin/src/app/shared/shared.module.ts
+++ bialydunajec-adminpanel-frontend/projects/bialydunajec-admin/src/app/shared/shared.module.ts
@@ -1,15 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule, registerLocaleData} from '@angular/common';
-import {NgZorroAntdModule, NZ_I18N, pl_PL} from 'ng-zorro-antd';
+import {NgZorroAntdModule, NZ_I18N, NZ_NOTIFICATION_CONFIG, pl_PL} from 'ng-zorro-antd';
 import pl from '@angular/common/locales/pl';
 import {FormInputComponent} from './component/form-input/form-input.component';
 import {BialyDunajecCommonsModule} from 'bialydunajec-commons';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { UserAvatarComponent } from './component/user-avatar/user-avatar.component';
+import {UserAvatarComponent} from './component/user-avatar/user-avatar.component';
 import {CampEditionSelectionComponent} from './component/camp-edition-selection/camp-edition-selection.component';
 import {HttpClientModule} from '@angular/common/http';
-import { PanelSectionComponent } from './component/panel-section/panel-section.component';
-import { BreadcrumbsComponent } from './component/breadcrumbs/breadcrumbs.component';
+import {PanelSectionComponent} from './component/panel-section/panel-section.component';
+import {BreadcrumbsComponent} from './component/breadcrumbs/breadcrumbs.component';
 
 registerLocaleData(pl);
 
@@ -41,7 +41,16 @@ registerLocaleData(pl);
     PanelSectionComponent,
     BreadcrumbsComponent
   ],
-  providers: [{provide: NZ_I18N, useValue: pl_PL}]
+  providers: [
+    {provide: NZ_I18N, useValue: pl_PL},
+    {
+      provide: NZ_NOTIFICATION_CONFIG, useValue: {
+        nzPlacement: 'topLeft',
+        nzDuration: 9000,
+        nzTop: '6rem'
+      }
+    }
+  ]
 })
 export class SharedModule {
 }
