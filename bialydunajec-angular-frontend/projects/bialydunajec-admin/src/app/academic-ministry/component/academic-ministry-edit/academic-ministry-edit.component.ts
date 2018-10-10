1176f185f43c7ec611e3104c4af78d8138c5194f
mnowak <mateusz.nowak@intenso.pl>
2018-10-10 09:32:18 +0200

    #20 Refactor academic-ministry-edit component.

--- bialydunajec-angular-frontend/projects/bialydunajec-admin/src/app/academic-ministry/component/academic-ministry-edit/academic-ministry-edit.component.ts
+++ bialydunajec-angular-frontend/projects/bialydunajec-admin/src/app/academic-ministry/component/academic-ministry-edit/academic-ministry-edit.component.ts
@@ -4,11 +4,13 @@ import {AcademicMinistryEndpoint} from '../../service/rest/academic-ministry.end
 import {AngularFormHelper} from '../../../../../../bialydunajec-main/src/app/shared/helper/angular-form.helper';
 import {HttpResponseHelper} from '../../../shared/helper/HttpResponseHelper';
 import {filter, finalize, flatMap, tap} from 'rxjs/operators';
-import {CreateAcademicMinistryRequest} from '../../service/rest/request/create-academic-ministry.request';
-import {ActivatedRoute} from '@angular/router';
+import {CreateAcademicMinistryRequest, UpdateAcademicMinistryRequest} from '../../service/rest/request/create-academic-ministry.request';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 import {AcademicMinistryResponse} from '../../service/rest/response/academic-ministry.response';
 import {AlertViewModel} from '../../../shared/view-model/ng-zorro/alert.view-model';
 import {EditFormMode} from './edit-form.mode';
+import {Observable, Observer} from 'rxjs';
+import {AcademicMinistryEditFormModel} from './academic-ministry-edit.form-model';
 
 @Component({
   selector: 'bda-admin-academic-ministry-edit',
@@ -22,6 +24,10 @@ export class AcademicMinistryEditComponent implements OnInit {
   lastAlert: AlertViewModel;
   submittingInProgress = false;
   academicMinistryForm: FormGroup;
+  // private formConfig = new Map<EditFormMode, {submitFn: () => Observable<any>, submitObserver: Observer<any>}>();
+  private formSubmitFn;
+  private formSubmitObservers;
+  private determineModeFn;
 
   constructor(
     private route: ActivatedRoute,
@@ -29,22 +35,60 @@ export class AcademicMinistryEditComponent implements OnInit {
     private academicMinistryEndpoint: AcademicMinistryEndpoint) {
   }
 
-  ngOnInit() {
-    this.initForm();
+  initFormSubmitFunctions(createSubmitFn: (...any) => Observable<any>, editSubmitFn: (...any) => Observable<any>) {
+    this.formSubmitFn = {};
+    this.formSubmitFn[EditFormMode.CREATE] = createSubmitFn;
+    this.formSubmitFn[EditFormMode.EDIT] = editSubmitFn;
+  }
+
+  initFormSubmitObservers(createSubmitObserver: Observer<any>, editSubmitObserver: Observer<any>) {
+    this.formSubmitObservers[EditFormMode.CREATE] = createSubmitObserver;
+    this.formSubmitObservers[EditFormMode.EDIT] = editSubmitObserver;
+  }
+
+  initDetermineModeFunction() {
+    this.determineModeFn = (activatedRouteParams: Params) => {
+      if (activatedRouteParams['academicMinistryId'] != null) {
+        return EditFormMode.EDIT;
+      } else {
+        return EditFormMode.CREATE;
+      }
+    };
+  }
+
+  loadInitFormValues() {
     this.route.params
       .pipe(
-        filter(params => params['academicMinistryId'] != null),
-        tap(_ => this.formMode = EditFormMode.EDIT),
+        tap(params => this.updateFormMode(params)),
         flatMap(params => this.academicMinistryEndpoint.getAcademicMinistryById(params['academicMinistryId']))
       )
       .subscribe(
         (response: AcademicMinistryResponse) => {
           console.log('Academic ministry:', response);
           this.currentAcademicMinistryId = response.academicMinistryId;
-          this.academicMinistryForm.patchValue({...response});
+          this.academicMinistryForm.patchValue(AcademicMinistryEditFormModel.fromDto(response));
           this.academicMinistryForm.updateValueAndValidity();
         }
       );
+  }
+
+  private updateFormMode(params) {
+    return this.formMode = this.determineModeFn(params);
+  }
+
+  ngOnInit() {
+    this.initDetermineModeFunction();
+    const submitPipeOperators = [finalize(() => this.submittingInProgress = false)];
+    this.initFormSubmitFunctions(
+      (createAcademicMinistryRequest: CreateAcademicMinistryRequest) =>
+        this.academicMinistryEndpoint.createAcademicMinistry(createAcademicMinistryRequest)
+          .pipe(...submitPipeOperators, tap(() => this.academicMinistryForm.reset())),
+      (academicMinistryId: string, updateAcademicMinistryRequest: UpdateAcademicMinistryRequest) =>
+        this.academicMinistryEndpoint.updateAcademicMinistryById(academicMinistryId, updateAcademicMinistryRequest)
+          .pipe(...submitPipeOperators)
+    );
+    this.initForm();
+    this.loadInitFormValues();
   }
 
   initForm() {
@@ -75,7 +119,7 @@ export class AcademicMinistryEditComponent implements OnInit {
       photoUrl: [null, []],
       description: this.formBuilder.group({
         title: [null, []],
-        content: [null, []]
+        content: [null, [Validators.required]]
       })
     });
   }
@@ -85,18 +129,15 @@ export class AcademicMinistryEditComponent implements OnInit {
     if (this.academicMinistryForm.valid) {
       this.lastAlert = null;
       this.submittingInProgress = true;
-      const createAcademicMinistryRequest: CreateAcademicMinistryRequest = {...this.academicMinistryForm.value};
       if (this.isEditMode) {
-        this.academicMinistryEndpoint.updateAcademicMinistryById(this.currentAcademicMinistryId, this.academicMinistryForm.value)
-          .pipe(
-            finalize(() => this.submittingInProgress = false)
-          )
+        const updateAcademicMinistryRequest: UpdateAcademicMinistryRequest = {...this.academicMinistryForm.value};
+        this.formSubmitFn[this.formMode](this.currentAcademicMinistryId, updateAcademicMinistryRequest)
           .subscribe(
             response => {
               console.log(response);
               this.lastAlert = {
                 type: 'success',
-                message: createAcademicMinistryRequest.officialName,
+                message: updateAcademicMinistryRequest.officialName,
                 description: 'zostało poprawnie zaktualizowane!'
               };
             },
@@ -107,34 +148,33 @@ export class AcademicMinistryEditComponent implements OnInit {
               if (HttpResponseHelper.isStatus4xx(response) && restErrors) {
                 this.lastAlert = {
                   type: 'error',
-                  message: createAcademicMinistryRequest.officialName,
+                  message: updateAcademicMinistryRequest.officialName,
                   description: 'nie zostało zaktualizowane, z powodu złamania reguł:' +
                     restErrors.map((e: string) => ` ${e}`)
                 };
               } else if (response.status === 0) {
                 this.lastAlert = {
                   type: 'error',
-                  message: createAcademicMinistryRequest.officialName,
+                  message: updateAcademicMinistryRequest.officialName,
                   description: 'nie zostało zaktualizowane, z powodu braku odpowiedzi serwera.'
                 };
               } else {
                 this.lastAlert = {
                   type: 'error',
-                  message: createAcademicMinistryRequest.officialName,
-                  description: `nie zostało zaktualizowane, z powodu błędu (jeśli nie wiesz co zrobić, to skontaktuj się z administratorem): \n ${error.message}`
+                  message: updateAcademicMinistryRequest.officialName,
+                  description: `nie zostało zaktualizowane, z powodu błędu 
+                  (jeśli nie wiesz co zrobić, to skontaktuj się z administratorem): \n ${error.message}`
                 };
               }
             }
           );
       } else {
-        this.academicMinistryEndpoint.createAcademicMinistry(this.academicMinistryForm.value)
-          .pipe(
-            finalize(() => this.submittingInProgress = false)
-          )
+        const createAcademicMinistryRequest: CreateAcademicMinistryRequest = {...this.academicMinistryForm.value};
+        this.formSubmitFn[this.formMode](createAcademicMinistryRequest)
           .subscribe(
             response => {
-              console.log(response);
-              this.academicMinistryForm.reset();
+              console.log('Create academic Ministry response:', response);
+              //this.academicMinistryForm.reset();
               this.lastAlert = {
                 type: 'success',
                 message: createAcademicMinistryRequest.officialName,
@@ -162,7 +202,8 @@ export class AcademicMinistryEditComponent implements OnInit {
                 this.lastAlert = {
                   type: 'error',
                   message: createAcademicMinistryRequest.officialName,
-                  description: `nie zostało utworzone, z powodu błędu (jeśli nie wiesz co zrobić, to skontaktuj się z administratorem): \n ${error.message}`
+                  description: `nie zostało utworzone, z powodu błędu 
+                  (jeśli nie wiesz co zrobić, to skontaktuj się z administratorem): \n ${error.message}`
                 };
               }
             }
