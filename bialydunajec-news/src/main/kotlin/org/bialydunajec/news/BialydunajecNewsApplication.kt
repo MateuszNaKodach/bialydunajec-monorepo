ad9858140557edf07786de157cfde393ccce8905
mnowak <mateusz.nowak@intenso.pl>
2018-10-18 18:09:17 +0200

    #7 Add bialydunajec-news module.

---
+++ bialydunajec-news/src/main/kotlin/org/bialydunajec/news/BialydunajecNewsApplication.kt
@@ -0,0 +1,11 @@
+package org.bialydunajec.news
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BialydunajecNewsApplication
+
+fun main(args: Array<String>) {
+    runApplication<BialydunajecNewsApplication>(*args)
+}
