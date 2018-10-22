3ba9e1587e6ebd96b3606e670c94a1f8158f93d0
mnowak <mateusz.nowak@intenso.pl>
2018-10-22 11:06:53 +0200

    #27 Add CampShirtSettingComponent.

--- bialydunajec-adminpanel-frontend/projects/bialydunajec-admin/src/app/camp-registrations/component/shirt-settings/shirt-settings.component.ts
+++ bialydunajec-adminpanel-frontend/projects/bialydunajec-admin/src/app/camp-registrations/component/shirt-settings/shirt-settings.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {CampEditionResponse} from '../../../camp-edition/service/rest/response/camp-edition.response';
+import {CampRegistrationsEndpoint} from '../../service/rest/camp-registrations.endpoint';
 
 @Component({
   selector: 'bda-admin-shirt-settings',
   templateUrl: './shirt-settings.component.html',
-  styleUrls: ['./shirt-settings.component.css']
+  styleUrls: ['./shirt-settings.component.less']
 })
 export class ShirtSettingsComponent implements OnInit {
 
-  constructor() { }
+  availableCampEditions: Observable<CampEditionResponse[]>;
+  currentCampEdition: number;
+
+
+  constructor(private campRegistrationsEndpoint: CampRegistrationsEndpoint) {
+  }
 
   ngOnInit() {
+    this.availableCampEditions = this.campRegistrationsEndpoint.getAllCampEditions();
+  }
+
+  onCampEditionIdSelected(selectedCampEditionId: number) {
+    this.currentCampEdition = selectedCampEditionId;
   }
 
 }
