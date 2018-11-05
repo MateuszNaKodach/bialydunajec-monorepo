649cf32a96bab3cff31668eda5d3fcc980426220
mnowak <mateusz.nowak@intenso.pl>
2018-11-05 22:00:29 +0100

    #27 Import FacebookModule from ngx-facebook.

---
+++ bialydunajec-adminpanel-frontend/projects/bialydunajec-main/src/app/shared/component/facebook/facebook-page/facebook-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bda-facebook-page',
+  templateUrl: './facebook-page.component.html',
+  styleUrls: ['./facebook-page.component.css']
+})
+export class FacebookPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
