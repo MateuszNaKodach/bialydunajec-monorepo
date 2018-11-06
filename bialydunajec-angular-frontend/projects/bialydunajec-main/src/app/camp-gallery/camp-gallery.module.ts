c79b3f91c8ca4f744e7538c9ee5b70e4ee4fac26
mnowak <mateusz.nowak@intenso.pl>
2018-11-06 10:08:56 +0100

    #29 Create Camp Gallery module.

---
+++ bialydunajec-angular-frontend/projects/bialydunajec-main/src/app/camp-gallery/camp-gallery.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CampGalleryRoutingModule } from './camp-gallery-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CampGalleryRoutingModule
+  ],
+  declarations: []
+})
+export class CampGalleryModule { }
