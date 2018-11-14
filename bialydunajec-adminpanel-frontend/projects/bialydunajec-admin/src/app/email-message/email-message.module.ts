320ea33b57235b98fee79196bd3955a2f140108d
mnowak <mateusz.nowak@intenso.pl>
2018-11-14 22:42:29 +0100

    #34 Add showing email history.

--- bialydunajec-adminpanel-frontend/projects/bialydunajec-admin/src/app/email-message/email-message.module.ts
+++ bialydunajec-adminpanel-frontend/projects/bialydunajec-admin/src/app/email-message/email-message.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { EmailMessageRoutingModule } from './email-message-routing.module';
+import { EmailHistoryComponent } from './component/email-history/email-history.component';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
+    SharedModule,
     EmailMessageRoutingModule
   ],
-  declarations: []
+  declarations: [EmailHistoryComponent]
 })
 export class EmailMessageModule { }
