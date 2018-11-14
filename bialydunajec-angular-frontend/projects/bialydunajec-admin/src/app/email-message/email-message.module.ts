fecfe72355f9fb2646953fd5a43194978ecb30c2
mnowak <mateusz.nowak@intenso.pl>
2018-11-14 19:51:30 +0100

    #34 Generate EmailMessage module.

---
+++ bialydunajec-angular-frontend/projects/bialydunajec-admin/src/app/email-message/email-message.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmailMessageRoutingModule } from './email-message-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    EmailMessageRoutingModule
+  ],
+  declarations: []
+})
+export class EmailMessageModule { }
