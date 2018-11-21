531d81f625d3cfd1708af61fc5c145b681415373
mnowak <mateusz.nowak@intenso.pl>
2018-11-21 09:57:11 +0100

    #38 Add CampBusModule.

---
+++ bialydunajec-angular-frontend/projects/bialydunajec-admin/src/app/camp-bus/camp-bus.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CampBusRoutingModule } from './camp-bus-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CampBusRoutingModule
+  ],
+  declarations: []
+})
+export class CampBusModule { }
--- bialydunajec-angular-frontend/projects/bialydunajec-admin/src/app/email-message/component/email-history/email-history.component.ts
+++ bialydunajec-angular-frontend/projects/bialydunajec-admin/src/app/email-message/component/email-history/email-history.component.ts
@@ -1,38 +1,85 @@
-import {Component, NgZone, OnInit} from '@angular/core';
+import {Component, NgZone, OnDestroy, OnInit} from '@angular/core';
 import {EmailMessageEndpoint} from '../../service/rest/email-message.endpoint';
 import {EmailMessageReadModel} from '../../service/rest/read-model/email-message.read-model';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {EmailStatisticsReadModel} from '../../service/rest/read-model/email-statistics.read-model';
 import {EventSourcePolyfill} from 'ng-event-source';
 import {environment} from '../../../../environments/environment';
+import {EventType} from '../../service/rest/event/event-type';
 
 @Component({
   selector: 'bda-admin-email-history',
   templateUrl: './email-history.component.html',
   styleUrls: ['./email-history.component.less']
 })
-export class EmailHistoryComponent implements OnInit {
+export class EmailHistoryComponent implements OnInit, OnDestroy {
 
-  emailMessages$: Observable<EmailMessageReadModel[]>;
+  private subscription: Subscription;
+  emailMessages: EmailMessageReadModel[];
   emailStatistics$: Observable<EmailStatisticsReadModel>;
+  newMessagesAvailable = false;
 
 
   constructor(private emailMessageEndpoint: EmailMessageEndpoint, private ngZone: NgZone) {
   }
 
-  ngOnInit() {
-    this.emailMessages$ = this.emailMessageEndpoint.getAllEmailMessage();
-    this.emailStatistics$ = this.emailMessageEndpoint.getEmailMessagesStatistics(); //TODO: Make emailStatistics Observable merge with eventsource
+  resendEmailMessage(emailMessageReadModel: EmailMessageReadModel) {
+    emailMessageReadModel.status = 'SENDING';
+    this.emailMessageEndpoint.resendEmailMessage(emailMessageReadModel.emailMessageLogId)
+      .subscribe();
+  }
 
-    const eventSource = new EventSourcePolyfill(`${environment.restApi.baseUrl}/rest-api/v1/admin/email-message/stream`,{});
+  forwardEmailMessage(emailMessageReadModel: EmailMessageReadModel) {
+    // this.emailMessageEndpoint.forwardEmailMessage(emailMessageReadModel.emailMessageLogId, {}).subscribe();
+  }
+
+  ngOnInit() {
+    this.loadEmailMessages();
+    this.loadEmailStatistics();
+    this.observeEmailMessagesProjectedEvents();
+  }
+
+  private observeEmailMessagesProjectedEvents() {
+    const eventSource = new EventSourcePolyfill(
+      `${environment.restApi.baseUrl}/rest-api/v1/admin/email-message/projected-events-stream`, {}
+    );
     console.log(eventSource);
     eventSource.onmessage = (event => {
+      const data: any = JSON.parse(event.data);
       /*this.ngZone.run(() => {
         console.log(event);
       });*/
-      this.emailMessages$ = this.emailMessageEndpoint.getAllEmailMessage();
-      this.emailStatistics$ = this.emailMessageEndpoint.getEmailMessagesStatistics();
+      switch (data.eventType) {
+        case EventType.EMAIL_MESSAGE_SENT_FAILURE: {
+          const emailMessage = this.emailMessages.find(it => it.emailMessageLogId === data.payload.emailMessageLogId);
+          emailMessage.status = 'FAIL_TO_SEND';
+          break;
+        }
+        case EventType.EMAIL_MESSAGE_SENT_SUCCESS: {
+          const emailMessage = this.emailMessages.find(it => it.emailMessageLogId === data.payload.emailMessageLogId);
+          emailMessage.status = 'SENT';
+          break;
+        }
+        case EventType.EMAIL_MESSAGE_CREATED: {
+          this.newMessagesAvailable = true;
+          break;
+        }
+      }
+      this.loadEmailStatistics();
     });
+  }
+
+  private loadEmailMessages() {
+    this.newMessagesAvailable = false;
+    this.subscription = this.emailMessageEndpoint.getAllEmailMessage().subscribe(messages => this.emailMessages = messages);
+  }
+
+  private loadEmailStatistics() {
+    this.emailStatistics$ = this.emailMessageEndpoint.getEmailMessagesStatistics();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
