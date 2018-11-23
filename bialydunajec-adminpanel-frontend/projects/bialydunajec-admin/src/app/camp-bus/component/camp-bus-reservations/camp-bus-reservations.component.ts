1e76955f7d0b621e1e70484e1a272867d6e437f8
mnowak <mateusz.nowak@intenso.pl>
2018-11-23 23:52:29 +0100

    Change existing .css files to .less.

--- bialydunajec-adminpanel-frontend/projects/bialydunajec-admin/src/app/camp-bus/component/camp-bus-reservations/camp-bus-reservations.component.ts
+++ bialydunajec-adminpanel-frontend/projects/bialydunajec-admin/src/app/camp-bus/component/camp-bus-reservations/camp-bus-reservations.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'bda-admin-camp-bus-reservations',
   templateUrl: './camp-bus-reservations.component.html',
-  styleUrls: ['./camp-bus-reservations.component.css']
+  styleUrls: ['./camp-bus-reservations.component.less']
 })
 export class CampBusReservationsComponent implements OnInit {
 
--- bialydunajec-adminpanel-frontend/projects/bialydunajec-admin/src/app/camp-bus/component/camp-bus-settings/camp-bus-settings.component.ts
+++ bialydunajec-adminpanel-frontend/projects/bialydunajec-admin/src/app/camp-bus/component/camp-bus-settings/camp-bus-settings.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'bda-admin-camp-bus-settings',
   templateUrl: './camp-bus-settings.component.html',
-  styleUrls: ['./camp-bus-settings.component.css']
+  styleUrls: ['./camp-bus-settings.component.less']
 })
 export class CampBusSettingsComponent implements OnInit {
 
