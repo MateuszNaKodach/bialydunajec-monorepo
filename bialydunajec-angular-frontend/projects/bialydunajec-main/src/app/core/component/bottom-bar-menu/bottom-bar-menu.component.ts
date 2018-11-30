54c0cb509b01284761304fed383cf8058f3397a7
mnowak <mateusz.nowak@intenso.pl>
2018-11-30 14:34:31 +0100

    #43 Add "more" option view in menu for small screens.

---
+++ bialydunajec-angular-frontend/projects/bialydunajec-main/src/app/core/component/bottom-bar-menu/bottom-bar-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bda-bottom-bar-menu',
+  templateUrl: './bottom-bar-menu.component.html',
+  styleUrls: ['./bottom-bar-menu.component.less']
+})
+export class BottomBarMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
