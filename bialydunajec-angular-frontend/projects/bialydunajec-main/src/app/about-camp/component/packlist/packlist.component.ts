32e37b2106fdaa06430a328ebc795460ba0f31cc
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2018-12-01 18:55:13 +0100

    #46 Add packlist component to FAQ.

---
+++ bialydunajec-angular-frontend/projects/bialydunajec-main/src/app/about-camp/component/packlist/packlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bda-packlist',
+  templateUrl: './packlist.component.html',
+  styleUrls: ['./packlist.component.scss']
+})
+export class PacklistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
