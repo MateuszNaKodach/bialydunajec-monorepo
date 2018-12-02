574ab03e8afd64a78a17c1d929379273b31c5a95
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2018-12-02 16:07:55 +0100

    #47 Add camp-participant-edit component.

---
+++ bialydunajec-adminpanel-frontend/projects/bialydunajec-admin/src/app/camp-registrations/component/camp-participant-edit/camp-participant-edit.component.ts
@@ -0,0 +1,100 @@
+import {Component, OnInit} from '@angular/core';
+import {EditFormMode} from '../../../academic-ministry/component/academic-ministry-edit/edit-form.mode';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Observable, Observer} from 'rxjs';
+import {ActivatedRoute, Params} from '@angular/router';
+import {finalize, tap} from 'rxjs/operators';
+import {
+  CreateAcademicMinistryRequest,
+  UpdateAcademicMinistryRequest
+} from '../../../academic-ministry/service/rest/request/create-academic-ministry.request';
+import {AcademicMinistryResponse} from '../../../academic-ministry/service/rest/response/academic-ministry.response';
+import {AcademicMinistryEditFormModel} from '../../../academic-ministry/component/academic-ministry-edit/academic-ministry-edit.form-model';
+import {AlertViewModel} from '../../../shared/view-model/ng-zorro/alert.view-model';
+import {CampRegistrationsEndpoint} from '../../service/rest/camp-registrations.endpoint';
+
+@Component({
+  selector: 'bda-admin-camp-participant-edit',
+  templateUrl: './camp-participant-edit.component.html',
+  styleUrls: ['./camp-participant-edit.component.less']
+})
+export class CampParticipantEditComponent implements OnInit {
+
+  // FORM UI
+  private formMode: EditFormMode = EditFormMode.CREATE;
+  campParticipantForm: FormGroup;
+  private formSubmitFn;
+  private formSubmitObservers;
+  private determineModeFn;
+  lastAlert: AlertViewModel;
+  submittingInProgress = false;
+
+
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, private campRegistrationsEndpoint: CampRegistrationsEndpoint) {
+  }
+
+  ngOnInit() {
+    this.initDetermineModeFunction();
+    const submitPipeOperators = [finalize(() => this.submittingInProgress = false)];
+    this.initFormSubmitFunctions(
+      (createAcademicMinistryRequest: CreateAcademicMinistryRequest) =>
+        this.academicMinistryEndpoint.createAcademicMinistry(createAcademicMinistryRequest)
+          .pipe(...submitPipeOperators, tap(() => this.academicMinistryForm.reset())),
+      (academicMinistryId: string, updateAcademicMinistryRequest: UpdateAcademicMinistryRequest) =>
+        this.academicMinistryEndpoint.updateAcademicMinistryById(academicMinistryId, updateAcademicMinistryRequest)
+          .pipe(...submitPipeOperators)
+    );
+    this.initForm();
+    this.loadInitFormValues();
+  }
+
+  initFormSubmitFunctions(createSubmitFn: (...any) => Observable<any>, editSubmitFn: (...any) => Observable<any>) {
+    this.formSubmitFn = {};
+    this.formSubmitFn[EditFormMode.CREATE] = createSubmitFn;
+    this.formSubmitFn[EditFormMode.EDIT] = editSubmitFn;
+  }
+
+  initFormSubmitObservers(createSubmitObserver: Observer<any>, editSubmitObserver: Observer<any>) {
+    this.formSubmitObservers[EditFormMode.CREATE] = createSubmitObserver;
+    this.formSubmitObservers[EditFormMode.EDIT] = editSubmitObserver;
+  }
+
+  initDetermineModeFunction() {
+    this.determineModeFn = (activatedRouteParams: Params) => {
+      if (activatedRouteParams['campParticipantId'] != null) {
+        return EditFormMode.EDIT;
+      } else {
+        return EditFormMode.CREATE;
+      }
+    };
+  }
+
+  loadInitFormValues() {
+    /*this.route.params
+      .pipe(
+        tap(params => this.updateFormMode(params)),
+        flatMap(params => this.academicMinistryEndpoint.getAcademicMinistryById(params['academicMinistryId']))
+      )
+      .subscribe(
+        (response: AcademicMinistryResponse) => {
+          console.log('Academic ministry:', response);
+          this.currentAcademicMinistryId = response.academicMinistryId;
+          this.academicMinistryForm.patchValue(AcademicMinistryEditFormModel.fromDto(response));
+          this.academicMinistryForm.updateValueAndValidity();
+          this.loadPriestsList();
+        }
+      );*/
+  }
+
+  private updateFormMode(params) {
+    return this.formMode = this.determineModeFn(params);
+  }
+
+  initForm() {
+
+  }
+
+  onSubmit() {
+
+  }
+}
---
+++ bialydunajec-adminpanel-frontend/projects/bialydunajec-main/src/app/shared/helper/request-error-observer.builder.ts
@@ -0,0 +1,45 @@
+import {RestErrorCode} from '../../campers-registration/service/rest/response/rest-error.code';
+import {HttpErrorResponse} from '@angular/common/http';
+import {HttpResponseHelper} from '../../../../../bialydunajec-admin/src/app/shared/helper/HttpResponseHelper';
+
+export class RequestErrorObserverBuilder {
+  private readonly restError: (restErrors: string[] | RestErrorCode[]) => any;
+  private readonly networkError: (error) => any;
+  private readonly unhandledError: (error) => any;
+
+  private static defaultCallback = error => console.log(error);
+
+  constructor(
+    restError: (restErrors: string[] | RestErrorCode[]) => any = RequestErrorObserverBuilder.defaultCallback,
+    unhandledError: (error) => any = RequestErrorObserverBuilder.defaultCallback,
+    networkError: (error) => any = RequestErrorObserverBuilder.defaultCallback) {
+    this.restError = restError;
+    this.networkError = networkError;
+    this.unhandledError = unhandledError;
+  }
+
+  static anyError(callback: (error) => any) {
+    return new RequestErrorObserverBuilder(callback, callback, callback);
+  }
+
+  getRequestErrorObserver() {
+    return (response: HttpErrorResponse) => {
+      const error = response.error;
+      const restErrors = response.error.restErrors;
+      if (HttpResponseHelper.isStatus4xx(response) && restErrors) {
+        console.log('REST ERROR');
+        this.restError(restErrors);
+      } else if (response.status === 0) {
+        console.log('NETWORK ERROR');
+        this.networkError(error);
+      } else {
+        console.log('UNHANDLED ERROR');
+        this.unhandledError(error);
+      }
+    };
+  }
+}
+
+
+
+
