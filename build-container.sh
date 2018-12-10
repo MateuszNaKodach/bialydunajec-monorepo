eb5452d12e2c57adc8fec95136ff83694756af69
mnowak <mateusz.nowak@intenso.pl>
2018-12-10 13:19:45 +0100

    #32 Add docker compose and build/ run scripts.

--- build-container.sh
+++ build-container.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-echo "==== Building the bialydunajec application"
-./gradlew build
-cp ./build/libs/bialydunajec-backend-0.0.1.2-SNAPSHOT.jar ./build/libs/bialydunajec-backend.jar
-
 echo "==== Building the bialydunajec-backend docker container image"
 docker build -t bialydunajec-backend -f Dockerfile .
 
---
+++ run-composed-containers.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "==== Starting the docker containers for whole application"
+docker-compose up
+
+echo "==== Docker compose is started!"
