2bc29157ccb9a1a726a46aed40df1b6b168808db
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2019-03-10 12:35:49 +0100

    #10 CircleCI - Configure deploy for all environments.
    * #10 CircleCI - Divide web app oauth clients to main and admin.
    
    * #10 CircleCI - Add encrypted prod passwords.
    
    * #10 CircleCI - Configure build and deploy workflow.
    
    * #10 CircleCI - Add Docker image to deploy job.

--- src/main/kotlin/org/bialydunajec/configuration/profile/ProfileNames.kt
+++ src/main/kotlin/org/bialydunajec/configuration/profile/ProfileNames.kt
@@ -4,12 +4,13 @@ class ProfileName {
     companion object{
         const val LOCAL_ENVIRONMENT = "env_local"
         const val DEVELOPMENT_ENVIRONMENT = "env_dev"
+        const val STAGING_ENVIRONMENT = "env_staging"
         const val PRODUCTION_ENVIRONMENT = "env_prod"
     }
 }
 
 class SubProfileName {
     companion object{
-        const val DATABASE_H2 = "database_h2"
+
     }
 }
