c67f07bb665a9b2a856cebd2b671155d824c0c00
MateuszNowak <kontakt.mateusznowak@gmail.com>
2019-04-04 12:47:46 +0200

    #74 Add configuration for development PostgreSQL.

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,4 +1,10 @@
-create table if not exists oauth_access_token
+
+CREATE SCHEMA IF NOT EXISTS camp_registrations;
+CREATE SCHEMA IF NOT EXISTS camp_edition;
+CREATE SCHEMA IF NOT EXISTS academic_ministry;
+CREATE SCHEMA IF NOT EXISTS users;
+CREATE SCHEMA IF NOT EXISTS email;
+CREATE TABLE IF NOT EXISTS oauth_access_token
 (
     token_id          VARCHAR(255) NOT NULL,
     token             BYTEA,
@@ -8,7 +14,7 @@ create table if not exists oauth_access_token
     authentication    BYTEA,
     refresh_token     VARCHAR(255)
 );
-create table if not exists oauth_refresh_token
+CREATE TABLE IF NOT EXISTS oauth_refresh_token
 (
     token_id       VARCHAR(255),
     token          BYTEA,
