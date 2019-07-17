fb18792e53cd97d7b7f4a67b1dcf7e0d00b58d96
mnowak <mateusz.nowak@intenso.pl>
2019-07-17 10:41:33 +0200

    Merge bialydunajec-camp-schedule presentation,domain,application and infrastructure in ddd-axon module.

--- bialydunajec-camp-schedule/ddd-axon/src/main/kotlin/org/bialydunajec/campschedule/eventsourcing/AggregateIdentifier.kt
+++ bialydunajec-camp-schedule/ddd-axon/src/main/kotlin/org/bialydunajec/campschedule/eventsourcing/AggregateIdentifier.kt
@@ -3,28 +3,28 @@ package org.bialydunajec.campschedule.eventsourcing
 import org.bialydunajec.ddd.domain.base.valueobject.Identifier
 import java.util.*
 
-abstract class EntityIdentifier(
-        private val entityId: String = EntityIdentifier.defaultValue()
+abstract class AggregateIdentifier(
+        private val aggregateId: String = defaultValue()
 ) : Identifier<String> {
 
-    override fun toString() = entityId
+    override fun toString() = aggregateId
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
-        other as EntityIdentifier
+        other as AggregateIdentifier
 
-        if (entityId != other.entityId) return false
+        if (aggregateId != other.aggregateId) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        return entityId.hashCode()
+        return aggregateId.hashCode()
     }
 
-    override fun getIdentifierValue() = entityId
+    override fun getIdentifierValue() = aggregateId
 
     companion object {
         fun defaultValue() = UUID.randomUUID().toString()
--- bialydunajec-camp-schedule/ddd-axon/src/main/kotlin/org/bialydunajec/campschedule/eventsourcing/EntityIdentifier.kt
+++ bialydunajec-camp-schedule/ddd-axon/src/main/kotlin/org/bialydunajec/campschedule/eventsourcing/EntityIdentifier.kt
@@ -3,28 +3,28 @@ package org.bialydunajec.campschedule.eventsourcing
 import org.bialydunajec.ddd.domain.base.valueobject.Identifier
 import java.util.*
 
-abstract class AggregateIdentifier(
-        private val aggregateId: String = AggregateIdentifier.defaultValue()
+abstract class EntityIdentifier(
+        private val entityId: String = defaultValue()
 ) : Identifier<String> {
 
-    override fun toString() = aggregateId
+    override fun toString() = entityId
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
-        other as AggregateIdentifier
+        other as EntityIdentifier
 
-        if (aggregateId != other.aggregateId) return false
+        if (entityId != other.entityId) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        return aggregateId.hashCode()
+        return entityId.hashCode()
     }
 
-    override fun getIdentifierValue() = aggregateId
+    override fun getIdentifierValue() = entityId
 
     companion object {
         fun defaultValue() = UUID.randomUUID().toString()
