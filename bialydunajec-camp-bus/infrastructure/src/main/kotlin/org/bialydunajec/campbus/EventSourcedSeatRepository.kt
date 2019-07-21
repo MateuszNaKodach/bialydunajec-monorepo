1e7d778c6b61c03f7453f68992f60dcbd5b42f76
mnowak <mateusz.nowak@intenso.pl>
2019-07-21 12:10:58 +0200

    #82 Upgrade kt-time-traveler dependency to 0.0.3 version.

--- bialydunajec-camp-bus/infrastructure/src/main/kotlin/org/bialydunajec/campbus/EventSourcedSeatRepository.kt
+++ bialydunajec-camp-bus/infrastructure/src/main/kotlin/org/bialydunajec/campbus/EventSourcedSeatRepository.kt
@@ -1,32 +1,38 @@
 package org.bialydunajec.campbus
 
 import arrow.mtl.instances.list.functorFilter.filter
+import com.github.nowakprojects.kttimetraveler.core.TimeProvider
 import org.bialydunajec.campbus.domain.Seat
 import org.bialydunajec.campbus.domain.SeatEvent
 import org.bialydunajec.campbus.domain.SeatId
 import org.bialydunajec.campbus.domain.SeatRepository
-import org.bialydunajec.eventsourcing.domain.TimeProvider
 import java.time.Instant
 import java.util.concurrent.ConcurrentHashMap
 
 typealias EventPublisher = (SeatEvent) -> Unit
 
-internal class EventSourcedSeatRepository(private val timeProvider: TimeProvider, private val eventPublisher: EventPublisher?) : SeatRepository {
+internal class EventSourcedSeatRepository(
+        private val timeProvider: TimeProvider,
+        private val eventPublisher: EventPublisher? = null)
+    : SeatRepository {
 
     private val seats = ConcurrentHashMap<SeatId, List<SeatEvent>>()
 
     override fun save(seat: Seat) {
+        if (seat.aggregateId.isUndefined()) {
+            throw IllegalArgumentException("Uninitialized aggregate cannot be saved!")
+        }
         val uncommittedChanges = seat.changes
-        val persistedChanges = getEventsUpTo(seat.aggregateId, timeProvider())
+        val persistedChanges = getEventsUpTo(seat.aggregateId, timeProvider.instant)
         seats[seat.aggregateId] = persistedChanges.plus(uncommittedChanges)
         uncommittedChanges.forEach { eventPublisher?.invoke(it) }
     }
 
     override fun findById(seatId: SeatId): Seat? =
-        when(seats.containsKey(seatId)){
-            true -> Seat.recreateFrom(timeProvider,getEventsUpTo(seatId,timeProvider()))
-            false -> null
-        }
+            when (seats.containsKey(seatId)) {
+                true -> Seat.recreateFrom({ timeProvider.instant }, getEventsUpTo(seatId, timeProvider.instant))
+                false -> null
+            }
 
 
     private fun getEventsUpTo(seatId: SeatId, timestamp: Instant) =
--- bialydunajec-camp-bus/infrastructure/src/test/kotlin/org/bialydunajec/campbus/EventSourcedSeatRepositorySpecification.kt
+++ bialydunajec-camp-bus/infrastructure/src/test/kotlin/org/bialydunajec/campbus/EventSourcedSeatRepositorySpecification.kt
@@ -1,4 +1,68 @@
 package org.bialydunajec.campbus
 
-object EventSourcedSeatRepositorySpecification {
+import assertk.assertThat
+import com.github.nowakprojects.kttimetraveler.test.TestClockTimeProvider
+import org.bialydunajec.campbus.domain.*
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
+import java.time.LocalTime
+import kotlin.test.assertFails
+
+object EventSourcedSeatRepositorySpecification : Spek({
+
+    Feature("time traveling with event sourcing") {
+
+        Scenario("events up to specific point in the past") {
+            val timeProvider: TestClockTimeProvider
+                    by memoized { TestClockTimeProvider.withFixedTime(LocalTime.of(10, 0)) }
+            val repository: SeatRepository by memoized { EventSourcedSeatRepository(timeProvider) }
+            val campBusCourseId: BusCourseId by memoized { BusCourseId() }
+            val seatId: SeatId by memoized { SeatId() }
+            val passengerId: PassengerId by memoized { PassengerId() }
+            val initialSeat: Seat by memoized { Seat.newInstance { timeProvider.instant }.handle(SeatCommand.AddSeatForCourse(seatId, campBusCourseId)) }
+
+            Given("current time is 10:00") {
+                timeProvider.timeTravelTo(LocalTime.of(10, 0))
+                println(timeProvider.instant)
+            }
+
+            And("seat is saved") {
+                repository.save(initialSeat)
+            }
+
+            When("we reserve the seat at 10:10") {
+                timeProvider.timeTravelTo(LocalTime.of(10, 10))
+                println(timeProvider.instant)
+
+                reserveBusSeat(repository, seatId, passengerId)
+            }
+
+            Then("if we load seat data with time set to 10:11 next reservation should fail") {
+                timeProvider.timeTravelTo(LocalTime.of(10, 11))
+                println(timeProvider.instant)
+                assertFails {
+                    reserveBusSeat(repository, seatId, passengerId)
+                }
+            }
+
+            And("if we load seat data with time set to 10:01 reservation should be possible") {
+                timeProvider.timeTravelTo(LocalTime.of(10, 1))
+                assertThat {
+                    reserveBusSeat(repository, seatId, passengerId)
+                }.doesNotThrowAnyException()
+            }
+
+
+        }
+
+    }
+})
+
+private fun reserveBusSeat(repository: SeatRepository, seatId: SeatId, passengerId: PassengerId) {
+    repository.findById(seatId)!!
+            .apply {
+                handle(SeatCommand.ReserveSeat(seatId, aggregateVersion, passengerId))
+                repository.save(this)
+            }
 }
+
