cefb82ffa04653fcdeed5236e12610b8774338a8
mnowak <mateusz.nowak@intenso.pl>
2019-08-06 20:07:57 +0200

    #71 Add credentials.json for Google Photos authentication.

--- bialydunajec-gallery/infrastructure/src/main/kotlin/org/bialydunajec/gallery/infrastructure/GooglePhotosClient.kt
+++ bialydunajec-gallery/infrastructure/src/main/kotlin/org/bialydunajec/gallery/infrastructure/GooglePhotosClient.kt
@@ -1,4 +1,40 @@
 package org.bialydunajec.gallery.infrastructure
 
+
 internal class GooglePhotosClient {
+/*
+    init {
+        val photosLibrarySettings = PhotosLibrarySettings.newBuilder()
+                .setCredentialsProvider(
+                        FixedCredentialsProvider.create(
+                                UserCredentials.create()
+                        )
+                )
+                .build()
+    }
+
+
+    @Throws(IOException::class, GeneralSecurityException::class)
+    private fun getUserCredentials(credentialsPath: String, selectedScopes: List<String>): Credentials {
+        val clientSecrets = GoogleClientSecrets.load(
+                JSON_FACTORY, InputStreamReader(FileInputStream(credentialsPath)))
+        val clientId = clientSecrets.getDetails().getClientId()
+        val clientSecret = clientSecrets.getDetails().getClientSecret()
+
+        val flow = GoogleAuthorizationCodeFlow.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                JSON_FACTORY,
+                clientSecrets,
+                selectedScopes)
+                .setDataStoreFactory(FileDataStoreFactory(DATA_STORE_DIR))
+                .setAccessType("offline")
+                .build()
+        val receiver = LocalServerReceiver.Builder().setPort(LOCAL_RECEIVER_PORT).build()
+        val credential = AuthorizationCodeInstalledApp(flow, receiver).authorize("user")
+        return UserCredentials.newBuilder()
+                .setClientId(clientId)
+                .setClientSecret(clientSecret)
+                .setRefreshToken(credential.getRefreshToken())
+                .build()
+    }*/
 }
