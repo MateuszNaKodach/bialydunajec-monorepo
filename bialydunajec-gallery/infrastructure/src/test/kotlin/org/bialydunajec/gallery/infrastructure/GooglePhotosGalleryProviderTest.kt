e04568e4c85b1c9eb72c40ce5433f4cfb2bf9f0e
Łukasz Michalczak <lukasz.michalczak@capgemini.com>
2019-08-20 16:47:13 +0200

    #71 test of googleProvider in spek

--- bialydunajec-gallery/infrastructure/src/test/kotlin/org/bialydunajec/gallery/infrastructure/GooglePhotosGalleryProviderTest.kt
+++ bialydunajec-gallery/infrastructure/src/test/kotlin/org/bialydunajec/gallery/infrastructure/GooglePhotosGalleryProviderTest.kt
@@ -1,105 +1,102 @@
 package org.bialydunajec.gallery.infrastructure
 
-import com.google.api.gax.core.FixedCredentialsProvider
-import com.google.auth.Credentials
+import assertk.assertThat
+import com.google.api.gax.rpc.UnauthenticatedException
 import com.google.photos.library.v1.PhotosLibraryClient
-import com.google.photos.library.v1.PhotosLibrarySettings
+import com.google.photos.library.v1.internal.InternalPhotosLibraryClient
+import groovy.transform.Undefined
 import io.mockk.*
-import org.junit.Test
+import org.bialydunajec.gallery.infrastructure.utils.GooglePhotosCredentialService
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.anyString
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
+import org.spockframework.util.ExceptionUtil
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.web.client.HttpClientErrorException
+import java.lang.Exception
 
-class GooglePhotosGalleryProviderTest {
+object GooglePhotosGalleryProviderTest: Spek( {
 
+    Feature("Fetching media from google photos gallery") {
 
-    @Test
-    fun getPhotosInAlbum() {
-        //given
-        val credentials = mockk<Credentials>(relaxed = true)
-        val photosLibrarySettings = PhotosLibrarySettings
-                .newBuilder()
-                .setCredentialsProvider(
-                        FixedCredentialsProvider.create(credentials)
-                ).build()
-        val photosLibraryClient = mockk<PhotosLibraryClient>(relaxed = true)
-        mockkObject(GooglePhotosCredentialService.Companion)
-        every { GooglePhotosCredentialService.initApiConnection()  } returns photosLibraryClient
-        val albumId = "albumId"
-        val googlePhotosGalleryProvider = GooglePhotosGalleryProvider()
+        Scenario("Fetching photos stored in album") {
+            val albumId = "albumId"
+            lateinit var googlePhotosGalleryProvider: GooglePhotosGalleryProvider
+            lateinit var photosLibraryClient: PhotosLibraryClient
 
-        //when
-        googlePhotosGalleryProvider.getPhotosInAlbum(albumId)
-
-        //then
-        verify(exactly = 1) { photosLibraryClient.searchMediaItems(albumId) }
-        confirmVerified(photosLibraryClient)
-    }
-
-    @Test
-    fun getAlbumListByCampEdition() {
-    }
-
-    @Test
-    fun getAlbumList() {
-    }
-
-    @Test
-    fun createAlbum() {
-    }
-}
-
-
-/*
-object EventSourcedSeatRepositorySpecification : Spek({
-
-    Feature("time traveling with event sourcing") {
-
-        Scenario("events up to specific point in the past") {
-            val timeProvider: TestClockTimeProvider
-                    by memoized { TestClockTimeProvider.withFixedTime(LocalTime.of(10, 0)) }
-            val repository: SeatRepository by memoized { InMemorySeatEventSourcedRepository(timeProvider) }
-            val campBusCourseId: BusCourseId by memoized { BusCourseId() }
-            val seatId: SeatId by memoized { SeatId() }
-            val passengerId: PassengerId by memoized { PassengerId() }
-            val initialSeat: Seat by memoized { Seat.newInstance { timeProvider.instant }.handle(SeatCommand.AddSeatForCourse(seatId, campBusCourseId)) }
-
-            Given("current time is 10:00") {
-                timeProvider.timeTravelTo(LocalTime.of(10, 0))
-                println(timeProvider.instant)
+            Given("Initialize connection"){
+                photosLibraryClient = mockk<PhotosLibraryClient>(relaxed = true)
+                mockkObject(GooglePhotosCredentialService)
+                every { GooglePhotosCredentialService.initApiConnection()  } returns photosLibraryClient
+                googlePhotosGalleryProvider = GooglePhotosGalleryProvider()
             }
 
-            And("seat is saved") {
-                repository.save(initialSeat)
+            lateinit var result: List<String>
+            When("Download photos stored in album") {
+                googlePhotosGalleryProvider.getPhotosInAlbum(albumId)
             }
 
-            When("we reserve the seat at 10:10") {
-                timeProvider.timeTravelTo(LocalTime.of(10, 10))
-                println(timeProvider.instant)
-                reserveBusSeat(repository, seatId, passengerId)
+            Then("Should invoke client method with albumId parameter and close connection") {
+                verify(exactly = 1) { photosLibraryClient.searchMediaItems(albumId) }
+                verify(exactly = 1) { photosLibraryClient.close() }
+                confirmVerified(photosLibraryClient)
+                assertThat{ result.isNotEmpty() }
+            }
+        }
+
+        Scenario("Fetching photos stored in album went wrong") {
+            val albumId = "albumId"
+            lateinit var googlePhotosGalleryProvider: GooglePhotosGalleryProvider
+            lateinit var photosLibraryClient: PhotosLibraryClient
+
+            Given("Initialize connection"){
+                photosLibraryClient = mockk<PhotosLibraryClient>(relaxed = true)
+                every { photosLibraryClient.searchMediaItems(ArgumentMatchers.anyString()) } throws IndexOutOfBoundsException()
+                mockkObject(GooglePhotosCredentialService)
+                every { GooglePhotosCredentialService.initApiConnection()  } returns photosLibraryClient
+                googlePhotosGalleryProvider = GooglePhotosGalleryProvider()
             }
 
-            Then("if we load seat data with time set to 10:11 next reservation should fail") {
-                timeProvider.timeTravelTo(LocalTime.of(10, 11))
-                println(timeProvider.instant)
-                assertThat(reserveBusSeat(repository, seatId, passengerId)).hasClass(SeatEvent.SeatReservationFailed::class)
+            lateinit var result: List<String>
+            When("Download photos stored in album") {
+                result = googlePhotosGalleryProvider.getPhotosInAlbum(albumId)
             }
 
-            And("if we load seat data with time set to 10:01 reservation should be possible") {
-                timeProvider.timeTravelTo(LocalTime.of(10, 1))
-                assertThat {
-                    reserveBusSeat(repository, seatId, passengerId)
-                }.doesNotThrowAnyException()
+            Then("Should invoke client method with albumId parameter and close connection") {
+                verify(exactly = 1) { photosLibraryClient.searchMediaItems(albumId) }
+                verify(exactly = 1) { photosLibraryClient.close() }
+                confirmVerified(photosLibraryClient)
+                assertThat{ result.isEmpty() }
+            }
+        }
+
+
+
+        Scenario("Fetching information of albums in google gallery") {
+            lateinit var googlePhotosGalleryProvider: GooglePhotosGalleryProvider
+            lateinit var photosLibraryClient: PhotosLibraryClient
+
+            Given("Initialize connection"){
+                photosLibraryClient = mockk<PhotosLibraryClient>(relaxed = true)
+                mockkObject(GooglePhotosCredentialService)
+                every { GooglePhotosCredentialService.initApiConnection()  } returns photosLibraryClient
+                googlePhotosGalleryProvider = GooglePhotosGalleryProvider()
             }
 
+            When("Download information of albums") {
+                googlePhotosGalleryProvider.getAlbumList()
+            }
 
+            Then("Should invoke client method and close connection") {
+                verify(exactly = 1) { photosLibraryClient.listAlbums() }
+                verify(exactly = 1) { photosLibraryClient.close() }
+                confirmVerified(photosLibraryClient)
+            }
         }
 
     }
 })
-
-private fun reserveBusSeat(repository: SeatRepository, seatId: SeatId, passengerId: PassengerId): SeatEvent {
-    val seat = repository.findById(seatId)!!.handle(SeatCommand.ReserveSeat(seatId, passengerId))
-    val lastEvent = seat.changes.last()
-    repository.save(seat)
-    return lastEvent
-}
-
- */
