42faadde21cb4c06dc6ef798716d8676579c9363
wojtek11530 <wojtek19962a32@gmail.com>
2019-09-11 12:41:08 +0200

    implement read admin controllers

---
+++ bialydunajec-email/read-model/src/main/kotlin/org/bialydunajec/email/readmodel/rest/v1/EmailAddressReadAdminController.kt
@@ -0,0 +1,40 @@
+package org.bialydunajec.email.readmodel.rest.v1
+
+import arrow.instances.list.applicative.map
+import org.bialydunajec.email.readmodel.*
+import org.bialydunajec.email.readmodel.EmailMessageLogEventStream
+import org.bialydunajec.email.readmodel.EmailMessageMongoRepository
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import kotlin.streams.toList
+
+@RequestMapping("/rest-api/v1/admin/email-address")
+@RestController
+internal class EmailAddressReadAdminController(
+        private val emailAddressMongoRepository: EmailAddressMongoRepository,
+        private val emailAddressCatalogGroupMongoRepository: EmailAddressCatalogGroupMongoRepository,
+        private val emailAddressStatisticsMongoRepository: EmailAddressStatisticsMongoRepository,
+        private val emailAddressEventStream: EmailAddressEventStream
+) {
+
+    @GetMapping
+    fun getAllEmailAddresses(): Collection<EmailAddress> = emailAddressMongoRepository.findAll()
+            .sortedByDescending { it.emailAddress }
+
+    @GetMapping("/statistics")
+    fun getEmailAddressStatistics() =
+            emailAddressStatisticsMongoRepository.findById(DEFAULT_EMAIL_ADDRESS_STATISTICS_ID)
+
+    @GetMapping(value = ["/projected-events-stream"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun getProjectedEventsStream() =
+            emailAddressEventStream.streamingEvents()
+
+    @GetMapping("/{groupName}")
+    fun getEmailAddressByGroupName(@PathVariable groupName: String) =
+            emailAddressMongoRepository.findAllById(
+                    emailAddressCatalogGroupMongoRepository.findAll().filter { it.groupName.equals(groupName) }.stream().map { it.emailAGroupId }.toList())
+
+}
