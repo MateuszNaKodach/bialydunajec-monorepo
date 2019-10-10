8bfd099f56b5bdeb4a8f0e113e2ea946088260f9
MateuszNowak <kontakt.mateusznowak@gmail.com>
2019-10-10 21:04:13 +0200

    #123 Version 0.0.2-SNAPSHOT

--- build-application.sh
+++ build-application.sh
@@ -3,6 +3,6 @@ set -e
 
 echo "==== Building the bialydunajec-backend application"
 ./gradlew build
-cp ./build/libs/bialydunajec-backend-0.0.1.2-SNAPSHOT.jar ./build/libs/bialydunajec-backend.jar
+cp ./build/libs/bialydunajec-backend-0.0.2-SNAPSHOT.jar ./build/libs/bialydunajec-backend.jar
 
 echo "==== bialydunajec-backend build!"
