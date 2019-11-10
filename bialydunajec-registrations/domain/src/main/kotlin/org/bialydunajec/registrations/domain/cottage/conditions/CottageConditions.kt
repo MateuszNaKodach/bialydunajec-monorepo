c0651291478014c91c5c8837300bae2623507516
Wojciech Okoński <wojteko22.96@gmail.com>
2019-11-10 13:51:47 +0100

    Add JPA annotations

--- bialydunajec-registrations/domain/src/main/kotlin/org/bialydunajec/registrations/domain/cottage/conditions/CottageConditions.kt
+++ bialydunajec-registrations/domain/src/main/kotlin/org/bialydunajec/registrations/domain/cottage/conditions/CottageConditions.kt
@@ -3,12 +3,19 @@ package org.bialydunajec.registrations.domain.cottage.conditions
 import org.bialydunajec.ddd.domain.base.aggregate.AggregateRoot
 import org.bialydunajec.ddd.domain.base.event.DomainEvent
 import org.bialydunajec.registrations.domain.cottage.CottageId
+import javax.persistence.ElementCollection
+import javax.persistence.Entity
+import javax.persistence.Table
 
+@Entity
+@Table(
+    schema = "camp_registrations"
+)
 class CottageConditions(cottageId: CottageId) : AggregateRoot<CottageId, DomainEvent<CottageId>>(cottageId) {
 
+    @ElementCollection
     var items: List<CottageConditionsDescriptionItem> = listOf()
         private set
-
 
     fun update(newConditionsDescription: List<CottageConditionsDescriptionItem>) {
         items = newConditionsDescription
--- bialydunajec-registrations/domain/src/main/kotlin/org/bialydunajec/registrations/domain/cottage/conditions/CottageConditionsDescriptionItem.kt
+++ bialydunajec-registrations/domain/src/main/kotlin/org/bialydunajec/registrations/domain/cottage/conditions/CottageConditionsDescriptionItem.kt
@@ -1,5 +1,7 @@
 package org.bialydunajec.registrations.domain.cottage.conditions
 
 import org.bialydunajec.ddd.domain.sharedkernel.valueobject.internet.Url
+import javax.persistence.Embeddable
 
+@Embeddable
 data class CottageConditionsDescriptionItem(val title: String, val content: String, val iconUrl: Url)
