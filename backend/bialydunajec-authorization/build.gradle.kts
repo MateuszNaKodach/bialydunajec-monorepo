604cad8dbe5a146245273f8d9b737ca8da452814
Mateusz Nowak <mateusz.nowak@vonage.com>
2020-08-26 23:00:32 +0200

    Spring + Gradle | BootRun fix

--- backend/bialydunajec-authorization/build.gradle.kts
+++ backend/bialydunajec-authorization/build.gradle.kts
@@ -1,9 +1,13 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
+import org.springframework.boot.gradle.tasks.run.BootRun
 
 version = "0.0.2"
 
 tasks {
     "bootJar"(BootJar::class) {
+        enabled = false
+    }
+    "bootRun"(org.springframework.boot.gradle.tasks.run.BootRun::class) {
         enabled = false
     }
     "jar"(Jar::class){
--- backend/bialydunajec-faq/build.gradle.kts
+++ backend/bialydunajec-faq/build.gradle.kts
@@ -1,7 +1,11 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
+import org.springframework.boot.gradle.tasks.run.BootRun
 
 tasks {
     "bootJar"(BootJar::class) {
+        enabled = false
+    }
+    "bootRun"(BootRun::class) {
         enabled = false
     }
     "jar"(Jar::class){
